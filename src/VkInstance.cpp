5cff8551f5515d69cafd52ea3cf031c4275de3ea
Su <z69500jqk@qq.com>
2023-11-18 13:27:31 +0800

    v1

---
+++ src/VkInstance.cpp
@@ -0,0 +1,8 @@
+﻿/**
+ * @File VkInstance.cpp
+ * @Author dfnzhc (https://github.com/dfnzhc)
+ * @Date 2023/11/18
+ * @Brief 
+ */
+
+#include "VkInstance.hpp"
---
+++ src/VkInstance.hpp
@@ -0,0 +1,17 @@
+﻿/**
+ * @File VkInstance.hpp
+ * @Author dfnzhc (https://github.com/dfnzhc)
+ * @Date 2023/11/18
+ * @Brief 
+ */
+
+#pragma once
+
+
+class VkInstance
+{
+public:
+    
+private:
+
+};
